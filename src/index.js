4c8b8595e3ebd58b5f2f520ea6a20879448a4af2
jofer86 <jofer86@gmail.com>
2019-08-20 11:45:46 -0500

    working hero, nav buttons, and menu content

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import {clicked} from "./onLoad";
+import {onload} from "./Content";
+
+const initializer = () => {
+  onload();
+  const clickEvent = document.querySelectorAll('.clickEvent');
+  _.each(clickEvent, (ele) => {
+    ele.addEventListener('click', (e) => clicked(e));
+  })
+}
+
+
+
+initializer();
