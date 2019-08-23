57590e57fab348a328c8b0a76d79c3cc5d52175d
jofer86 <jofer86@gmail.com>
2019-08-23 13:32:43 -0500

    DRY stuff

--- src/index.js
+++ src/index.js
@@ -6,21 +6,23 @@ import Find from './Find'
 
 const clicked = (e) => {
   const target = e.srcElement
+  const cleanup = () => {
+    document.getElementById(1).classList.remove('clicked')
+    document.getElementById(2).classList.remove('clicked')
+    document.getElementById(3).classList.remove('clicked')
+  }
   if (target.id === '1') {
     Menu.render()
-    document.getElementById(3).classList.remove('clicked')
-    document.getElementById(2).classList.remove('clicked')
+    cleanup()
     document.getElementById(1).classList.add('clicked')
   } else if (target.id === '2') {
     Contact.render()
+    cleanup()
     document.getElementById(2).classList.add('clicked')
-    document.getElementById(1).classList.remove('clicked')
-    document.getElementById(3).classList.remove('clicked')
   } else {
     Find.render()
-    document.getElementById(1).classList.remove('clicked')
-    document.getElementById(3).classList.add('clicked')
-    document.getElementById(2).classList.remove('clicked')
+    cleanup()
+    document.getElementById(3).classList.add('clicked')    
   }
 }
 
